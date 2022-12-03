160a509f9bc0486ecd93d2cc203b55efdbc0c4e8
Guy Waldman <6546430+guywaldman@users.noreply.github.com>
2022-12-04 00:51:34 +0200

    Add solution for part 01

--- day-01/rust/src/part_01.rs
+++ day-01/rust/src/part_01.rs
@@ -1,3 +1,36 @@
 pub fn solve_part_01<'a>(input: &'a str) -> String {
-    input.len().to_string()
+    let max_carried_per_elf: usize = input
+        .split("\n\n")
+        .map(|txt| {
+            txt.lines()
+                .map(|weight| weight.trim().parse::<usize>().unwrap_or(0))
+                .sum()
+        })
+        .max()
+        .unwrap();
+
+    max_carried_per_elf.to_string()
+}
+
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_01() {
+        let input = "1000
+2000
+3000
+
+4000
+
+5000
+6000
+
+7000
+8000
+9000
+
+10000";
+        assert_eq!(solve_part_01(input), "24000");
+    }
 }
