fedbf4a61279bd108bf05bdd9196f1dc698debfb
Guy Waldman <6546430+guywaldman@users.noreply.github.com>
2022-12-04 01:06:11 +0200

    Consolidate inputs

--- 2022/day-01/rust/src/bin/part_02_sol.rs
+++ 2022/day-01/rust/src/bin/part_02_sol.rs
@@ -1,6 +1,6 @@
 use day_01::part_02::solve_part_02;
 
 fn main() {
-    let input = include_str!("../../inputs/part_02_input.txt");
+    let input = include_str!("../../inputs/input.txt");
     println!("Solution: {}", solve_part_02(&input));
 }
